6c6d0b310b99420833b4252940b9a1143c196f65
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-08-23 21:03:40 +0900

    crop model map for gl5

---
+++ map/bin/calc_crpfrc_gl5.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+############################################################
+#to   prepare mosaic fraction by crop land use
+#by   2010/09/30, hanasaki, NIES: H08ver1.0
+############################################################
+# Geographical settings (Edit here if you change spatial domain/resolution)
+############################################################
+L=$LGL5 #259200
+XY=$XYGL5 #"720 360"
+L2X=$L2XGL5 #../../map/dat/l2x_l2y_/l2x.hlf.txt
+L2Y=$L2YGL5 #../../map/dat/l2x_l2y_/l2y.hlf.txt
+LONLAT="-180 180 -90 90"
+ARG="$L $XY $L2X $L2Y $LONLAT"
+SUF=.gl5 #.hlf
+MAP=.WFDEI #.WATCH
+############################################################
+# Basic setting (Edit here if you wish)
+############################################################
+OPT="double"     # double or single
+PRJRUNOUT=S05_____
+############################################################
+# Input (Edit here if you wish)
+############################################################
+IRGARA=../../map/dat/irg_ara_/S05_____20000000${SUF}  # Irrigated area
+IRGEFF=../../map/dat/irg_eff_/DS02____00000000${SUF}  # Irrigat. effic.
+CRPINT=../../map/dat/crp_int_/DS02____00000000${SUF}  # Crop intensity
+
+LNDMSK=../../map/dat/lnd_msk_/lndmsk${MAP}${SUF}     # land mask
+LNDARA=../../map/dat/lnd_ara_/lndara${MAP}${SUF}     # land area
+CRPARA=../../map/dat/crp_ara_/R08_____20000000${SUF} # cropland area
+############################################################
+# Output (Edit here if you wish)
+############################################################
+DIRCRPINT1ST=../../map/out/crp_int1          # crop intensity of 1st crop
+DIRCRPINT2ND=../../map/out/crp_int2          # crop intensity of 2nd crop
+DIRIRGARADBL=../../map/out/irg_arad          # irrigated area for double crop
+DIRIRGARASGL=../../map/out/irg_aras          # irrigated area for single crop
+   DIRRFDARA=../../map/out/rfd_ara_          # rainfed cropland area 
+   DIRNONARA=../../map/out/non_ara_          # non cropland area
+DIRIRGFRCDBL=../../map/out/irg_frcd          # land area fraction for d crop
+DIRIRGFRCSGL=../../map/out/irg_frcs          # land area fraction for s crop
+   DIRRFDFRC=../../map/out/rfd_frc_          # land area fraction rainfed
+   DIRNONFRC=../../map/out/non_frc_          # land area fraction non crop
+#
+CRPINT1ST=${DIRCRPINT1ST}/${PRJRUNOUT}${YEAR}0000${SUF}
+CRPINT2ND=${DIRCRPINT2ND}/${PRJRUNOUT}${YEAR}0000${SUF}
+IRGARADBL=${DIRIRGARADBL}/${PRJRUNOUT}20000000${SUF}
+IRGARASGL=${DIRIRGARASGL}/${PRJRUNOUT}20000000${SUF}
+   RFDARA=${DIRRFDARA}/${PRJRUNOUT}20000000${SUF}
+   NONARA=${DIRNONARA}/${PRJRUNOUT}20000000${SUF}
+IRGFRCDBL=${DIRIRGFRCDBL}/${PRJRUNOUT}20000000${SUF}
+IRGFRCSGL=${DIRIRGFRCSGL}/${PRJRUNOUT}20000000${SUF}
+   RFDFRC=${DIRRFDFRC}/${PRJRUNOUT}20000000${SUF}
+   NONFRC=${DIRNONFRC}/${PRJRUNOUT}20000000${SUF}
+#
+LOG=temp.log
+############################################################
+# Job (prepare directory)
+############################################################
+if [ !  -f $DIRCRPINT1ST ]; then  mkdir -p $DIRCRPINT1ST; fi
+if [ !  -f $DIRCRPINT2ND ]; then  mkdir -p $DIRCRPINT2ND; fi
+if [ !  -f $DIRIRGARADBL ]; then  mkdir -p $DIRIRGARADBL; fi
+if [ !  -f $DIRIRGARASGL ]; then  mkdir -p $DIRIRGARASGL; fi
+if [ !  -f $DIRRFDARA    ]; then  mkdir -p $DIRRFDARA; fi
+if [ !  -f $DIRNONARA    ]; then  mkdir -p $DIRNONARA; fi
+if [ !  -f $DIRIRGFRCDBL ]; then  mkdir -p $DIRIRGFRCDBL; fi
+if [ !  -f $DIRIRGFRCSGL ]; then  mkdir -p $DIRIRGFRCSGL; fi
+if [ !  -f $DIRRFDFRC    ]; then  mkdir -p $DIRRFDFRC; fi
+if [ !  -f $DIRNONFRC    ]; then  mkdir -p $DIRNONFRC; fi
+############################################################
+# Job (crop intensity)
+############################################################
+prog_crpint $L $CRPINT $CRPINT1ST $CRPINT2ND
+############################################################
+# Job (area & fraction for standard double crops)
+############################################################
+if [ $OPT = "double" ]; then
+# area of irrigation double
+  htmath $L mul $IRGARA $CRPINT2ND $IRGARADBL
+  echo Irrigated area for double crop: `htstat $ARG sum $IRGARADBL` >  $LOG
+# area of irrigation single
+  htmath $L mul $IRGARA    $CRPINT1ST $IRGARASGL
+  htmath $L sub $IRGARASGL $IRGARADBL $IRGARASGL
+  echo Irrigated area for single crop: `htstat $ARG sum $IRGARASGL` >> $LOG
+# area of rainfed
+  htmath $L sub $CRPARA    $IRGARASGL $RFDARA
+  htmath $L sub $RFDARA    $IRGARADBL $RFDARA
+  htmaskrplc $ARG   $RFDARA    $RFDARA lt 0.0 0.0 $RFDARA    > /dev/null
+  echo Rainfed area:       `htstat $ARG sum $RFDARA`         >> $LOG
+# area of non-cropland
+  htmath $L sub $LNDARA $IRGARASGL $NONARA
+  htmath $L sub $NONARA $IRGARADBL $NONARA
+  htmath $L sub $NONARA $RFDARA    $NONARA
+  echo Non-cropland area: `htstat $ARG sum $NONARA`          >> $LOG
+# fraction of irrigation single
+  htmath $L div $IRGARASGL $LNDARA $IRGFRCSGL
+  echo Irrig area fraction max single: `htstat $ARG max $IRGFRCSGL` >> $LOG
+  echo Irrig area fraction min single: `htstat $ARG min $IRGFRCSGL` >> $LOG
+# fraction of irrigation double
+  htmath $L div $IRGARADBL $LNDARA $IRGFRCDBL
+  echo Irrig area fraction max double: `htstat $ARG max $IRGFRCDBL` >> $LOG
+  echo Irrig area fraction min double: `htstat $ARG min $IRGFRCDBL` >> $LOG
+# fraction of rainfed
+  htmath $L div $RFDARA    $LNDARA $RFDFRC
+  echo Rainfed area fraction max: `htstat $ARG max $RFDFRC`  >> $LOG
+  echo Rainfed area fraction min: `htstat $ARG min $RFDFRC`  >> $LOG
+# fraction of non-cropland
+  htmath $L div $NONARA   $LNDARA $NONFRC
+  htmask $ARG $NONFRC $LNDMSK eq 1 $NONFRC   > /dev/null
+  echo Non-crop area fraction max: `htstat $ARG max $NONFRC` >> $LOG
+  echo Non-crop area fraction min: `htstat $ARG min $NONFRC` >> $LOG
+elif [ $OPT = "single" ]; then
+# area of irrigation single
+  htmath $L mul $IRGARA    $CRPINT1ST $IRGARASGL
+  echo Irrigated area:     `htstat $ARG sum $IRGARASGL`      >> $LOG
+# area of rainfed
+  htmath $L sub $CRPARA    $IRGARASGL $RFDARA
+  htmaskrplc $ARG   $RFDARA    $RFDARA lt 0.0 0.0 $RFDARA
+  echo Rainfed area:       `htstat $ARG sum $RFDARA`         >> $LOG
+# area of non-cropland
+  htmath $L sub $LNDARA $IRGARASGL $NONARA
+  htmath $L sub $NONARA $RFDARA    $NONARA
+  echo Non-cropland area: `htstat $ARG sum $NONARA`          >> $LOG
+# fraction of irrigation single
+  htmath $L div $IRGARASGL $LNDARA $IRGFRCSGL
+  echo Irrig area fraction max: `htstat $ARG max $IRGFRCSGL` >> $LOG
+  echo Irrig area fraction min: `htstat $ARG min $IRGFRCSGL` >> $LOG
+# fraction of rainfed
+  htmath $L div $RFDARA    $LNDARA $RFDFRC
+  echo Irrig area fraction max: `htstat $ARG max $IRGFRCSGL` >> $LOG
+  echo Irrig area fraction min: `htstat $ARG min $IRGFRCSGL` >> $LOG
+# fraction of non-cropland
+  htmath $L div $NONARA   $LNDARA $NONFRC
+  htmask $ARG $NONFRC $LNDMSK eq 1 $NONFRC   > /dev/null
+  echo Non-crop area fraction max: `htstat $ARG max $NONFRC` >> $LOG
+  echo Non-crop area fraction min: `htstat $ARG min $NONFRC` >> $LOG
+fi
---
+++ map/pre/prep_crp_DS02_gl5.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+############################################################
+#to   prepare Doell and Siebert 2002 irrigation map
+#by   2010/09/30, hanasaki, NIES: H08ver1.0
+#
+# Source:
+#
+# Doll, P., and S. Siebert (2002),
+# Global modeling of irrigation water requirements,
+# Water Resour. Res., 38(4). 1037, doi:10.1029/2001WR000355
+#
+############################################################
+# Geography (Edit here, but ONLY one degree global OR half degree global)
+############################################################
+L=259200
+XY="720 360"
+L2X=../../map/dat/l2x_l2y_/l2x.hlf.txt
+L2Y=../../map/dat/l2x_l2y_/l2y.hlf.txt
+LONLAT="-180 180 -90 90"
+ARG="$L $XY $L2X $L2Y $LONLAT"
+SUF=.hlf
+MAP=.WFDEI #.WATCH
+MAP1=.WFDEI
+############################################################
+# Setting (Do not edit here unless you are an expert)
+############################################################
+PRJ=DS02
+RUN=____
+YEAR=0000
+############################################################
+# Input (Do not edit here unless you are an expert)
+############################################################
+IRGARAORG=../../map/org/DS02/irgara.DS02${SUF}       # Irrigated area
+IRGEFFORG=../../map/org/DS02/irgeff.DS02${SUF}       # Irrigated efficiency
+CRPINTORG=../../map/org/DS02/crpint.DS02${SUF}       # Crop intensity
+#
+LNDMSK=../../map/dat/lnd_msk_/lndmsk${MAP}${SUF}     # land mask
+LNDMSKGL5=../../map/dat/lnd_msk_/lndmsk${MAP1}.gl5 #${SUF}     # land mask gl5
+
+############################################################
+# Output (Do not edit here unless you are an expert)
+############################################################
+DIRIRGARA=../../map/dat/irg_ara_            # irrigated area
+DIRIRGEFF=../../map/dat/irg_eff_            # irrigation efficiency
+DIRCRPINT=../../map/dat/crp_int_            # crop intensity
+#
+IRGARA=${DIRIRGARA}/${PRJ}${RUN}${YEAR}0000${SUF}
+IRGEFF=${DIRIRGEFF}/${PRJ}${RUN}${YEAR}0000${SUF}
+CRPINT=${DIRCRPINT}/${PRJ}${RUN}${YEAR}0000${SUF}
+#
+IRGARAGL5=${DIRIRGARA}/${PRJ}${RUN}${YEAR}0000.gl5 #${SUF}
+IRGEFFGL5=${DIRIRGEFF}/${PRJ}${RUN}${YEAR}0000.gl5 #${SUF}
+CRPINTGL5=${DIRCRPINT}/${PRJ}${RUN}${YEAR}0000.gl5 #${SUF}
+############################################################
+# Job (prepare directory)
+############################################################
+if [ !  -f $DIRIRGARA    ]; then  mkdir -p $DIRIRGARA; fi
+if [ !  -f $DIRIRGEFF    ]; then  mkdir -p $DIRIRGEFF; fi
+if [ !  -f $DIRCRPINT    ]; then  mkdir -p $DIRCRPINT; fi
+############################################################
+# Job (original --> hformat)
+# - irrigated area
+# - crop intensity
+# - irrigated efficiency
+############################################################
+htformat $ARG asciiu binary $IRGARAORG.txt $IRGARAORG
+echo Irrigated area: `htstat $ARG sum $IRGARAORG`
+#
+htformat $ARG asciiu binary $CRPINTORG.txt $CRPINTORG
+echo Crop intensity: `htstat $ARG sum $CRPINTORG`
+#
+htformat $ARG asciiu binary $IRGEFFORG.txt $IRGEFFORG
+echo Irrigated efficiency: `htstat $ARG sum $IRGEFFORG`
+############################################################
+# Job (reshape original data)
+# - irrigated area
+# - crop intensity
+# - irrigated efficiency
+############################################################
+htmask $ARG $IRGARAORG $LNDMSK eq 1 $IRGARA  > /dev/null
+echo Irrigated area: `htstat $ARG sum $IRGARA`
+#
+htmask $ARG $CRPINTORG $LNDMSK eq 1 $CRPINT > /dev/null
+echo Crop intensity: `htstat $ARG sum $CRPINT`
+#
+htmask $ARG $IRGEFFORG $LNDMSK eq 1 $IRGEFF > /dev/null
+echo Irrigated efficiency: `htstat $ARG sum $IRGEFF`
+############################################################ gl5 @menaka
+prog_hlf2gl5 $L2X $L2Y $L2XGL5 $L2YGL5 $IRGARA $IRGARAGL5
+htmath $LGL5 div $IRGARAGL5 36 $IRGARAGL5 # divid by 36 grid uniform distribution  
+prog_hlf2gl5 $L2X $L2Y $L2XGL5 $L2YGL5 $IRGEFF $IRGEFFGL5
+prog_hlf2gl5 $L2X $L2Y $L2XGL5 $L2YGL5 $CRPINT $CRPINTGL5
+############################################################
+htmask $ARGGL5 $IRGARAGL5 $LNDMSKGL5 eq 1  $IRGARAGL5  > /dev/null
+echo Irrigated area: `htstat $ARGGL5 sum $IRGARAGL5`
+#
+htmask $ARGGL5 $CRPINTGL5 $LNDMSKGL5 eq 1 $CRPINTGL5 > /dev/null
+echo Crop intensity: `htstat $ARGGL5 sum $CRPINTGL5`
+#
+htmask $ARGGL5 $IRGEFFGL5 $LNDMSKGL5 eq 1 $IRGEFFGL5 > /dev/null
+echo Irrigated efficiency: `htstat $ARGGL5 sum $IRGEFFGL5`

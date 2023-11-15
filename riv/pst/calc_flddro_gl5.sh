0efa4f1002d91f505bbbae76082044ba475d3a51
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-11-15 10:59:34 +0900

    resorvoir model

---
+++ riv/pst/calc_flddro_gl5.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+############################################################
+#to   calculate operational year 
+#by   2010/06/30, hanasaki, NIES: H08ver1.0
+############################################################
+# Basic settings (Edit here)
+############################################################
+PRJ=W5E5          #
+RUN=LR__          #
+YEARMIN=0000      #
+YEARMAX=0000      #
+LDBG=27641        #
+PROG=prog_flddro  #
+#
+LOG=temp.log
+############################################################
+# Geographical settings (Edit here if you change spatial domain/resolution)
+############################################################
+L=933120          #
+SUF=.gl5          #
+############################################################
+# Input (Do not edit here basically)
+############################################################
+RIVOUTMON=../../riv/out/riv_out_/$PRJ$RUN${SUF}MO
+RIVOUTANU=../../riv/out/riv_out_/$PRJ$RUN${SUF}YR
+############################################################
+# Output directory (Do not edit here basically)
+############################################################
+  DIRFLGFLD=../../riv/out/fld_dro_
+ DIRFLD2DRO=../../riv/out/fld2dro_
+ DIRDRO2FLD=../../riv/out/dro2fld_
+  DIRFLDDUR=../../riv/out/fld_dur_
+  DIRFLDRAT=../../riv/out/fld_rat_
+############################################################
+# Output (Do not edit here basically)
+############################################################
+     FLGFLD=$DIRFLGFLD/$PRJ$RUN${SUF}YR
+    FLD2DRO=$DIRFLD2DRO/$PRJ$RUN${SUF}YR
+    DRO2FLD=$DIRDRO2FLD/$PRJ$RUN${SUF}YR
+     FLDDUR=$DIRFLDDUR/$PRJ$RUN${SUF}YR
+     FLDRAT=$DIRFLDRAT/$PRJ$RUN${SUF}YR
+############################################################
+# Job (Prepare directory)
+############################################################
+  if [ ! -d $DIRFLGFLD  ]; then mkdir -p $DIRFLGFLD;  fi
+  if [ ! -d $DIRFLD2DRO ]; then mkdir -p $DIRFLD2DRO; fi
+  if [ ! -d $DIRDRO2FLD ]; then mkdir -p $DIRDRO2FLD; fi
+  if [ ! -d $DIRFLDDUR  ]; then mkdir -p $DIRFLDDUR;  fi
+  if [ ! -d $DIRFLDRAT  ]; then mkdir -p $DIRFLDRAT;  fi
+############################################################
+#
+############################################################
+YEAR=$YEARMIN
+while [ $YEAR -le $YEARMAX ]; do
+  $PROG $L $YEAR $LDBG $RIVOUTMON $RIVOUTANU $FLGFLD $FLD2DRO $DRO2FLD $FLDDUR $FLDRAT > $LOG
+  YEAR=`expr $YEAR + 1`
+done

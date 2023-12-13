f15c42e0e494031b44f20f06d75273b12e4c2878
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2023-12-13 15:41:28 +0900

    city mask completed

--- map/dat/make_citymask.py
+++ map/dat/make_citymask.py
@@ -1,351 +1,229 @@
 import numpy as np
 
-MAP='CAMA'
+def explore_citymask(index, threshold_density=1000):
+    # index=23: Jakarta
 
-pop_list = []
-name_list = []
-inf_path = '../../map/dat/cty_lst_/city_list03.txt'
-for l in open(inf_path).readlines():
-    data = l[:-1].split('	')
-    pop_list.append(int(data[4]))
-    name_list.append(data[5])
+    #-----------------------------------------------
+    # Initialization
+    #-----------------------------------------------
 
-pop = np.fromfile('../../map/dat/pop_tot_/C05_a___20000000.gl5', 'float32').reshape(2160, 4320)
-area = np.fromfile(f'../../map/dat/lnd_ara_/lndara.{MAP}.gl5', 'float32').reshape(2160, 4320)
-pop_d = (pop/area/1000./1000.)
+    # map data
+    MAP='CAMA'
 
-#-----------------------------------------------
-for num in range(1,901,1):   ##cheak city number##
+    # initial radius from central point
+    radius = 1
 
-    tt_1 = 1000
-    tt_2 = 2000
-    tt_3 = 3000
-    location = np.fromfile(f'../../map/dat/cty_cnt_/city_{num:08d}.gl5','float32').reshape(2160,4320)
-    pop_city = pop_list[num-1]*1000
-    city_name = name_list[num-1]
+    # explore grid radius
+    radius_max = 12
+
+    #-----------------------------------------------
+    # load true data (UN city list)
+    #-----------------------------------------------
+
+    pop_list = []
+    name_list = []
+    inf_path = '/home/kajiyama/H08/H08_20230612/map/dat/cty_lst_/city_list03.txt'
+    for l in open(inf_path).readlines():
+        data = l[:-1].split('	')
+        pop_list.append(int(data[4]))
+        name_list.append(data[5])
+
+    #-----------------------------------------------
+    # load population and ared data
+    #-----------------------------------------------
+
+    # population data(GWP4 2000)
+    pop = np.fromfile('/home/kajiyama/H08/H08_20230612/map/dat/pop_tot_/C05_a___20000000.gl5', 'float32').reshape(2160, 4320)
+    area = np.fromfile(f'/home/kajiyama/H08/H08_20230612/map/dat/lnd_ara_/lndara.{MAP}.gl5', 'float32').reshape(2160, 4320)
+
+    #-----------------------------------------------
+    # load city_center coordinate
+    #-----------------------------------------------
+
+    location = np.fromfile(f'/home/kajiyama/H08/H08_20230612/map/dat/cty_cnt_/city_{index:08d}.gl5','float32').reshape(2160,4320)
     x = np.where(location==1)[0]
     y = np.where(location==1)[1]
     x = x[0]
     y = y[0]
-    tt_list = [1000,2000,3000]
 
-    #-------------------ROUND 1----------------------
+    #-----------------------------------------------
+    # check if city center is right
+    #-----------------------------------------------
+    org_cnt = pop[x, y]
+    circle=3
+    for a in range(x-circle, x+circle+1):
+        for b in range(y-circle, y+circle+1):
+            candidate = pop[a, b]
+            if candidate >= org_cnt:
+                org_cnt = candidate
+                x = a
+                y = b
+
+    #-----------------------------------------------
+    #  Get city population and city name
+    #-----------------------------------------------
+
+    pop_city = pop_list[index-1]*1000
+    city_name = name_list[index-1]
+
+    #-----------------------------------------------
+    #  Get area(km2) and population per grid
+    #-----------------------------------------------
+
     A = area[x,y]/1000./1000.
-    thre_1 = tt_1*A
-    thre_2 = tt_2*A
-    thre_3 = tt_3*A
+    threshold = threshold_density * A
 
-    mask_1 = np.zeros((2160,4320),'float32')
-    mask_1[x,y] = 1
-    mask_2 = np.zeros((2160,4320),'float32')
-    mask_2[x,y] = 1
-    mask_3 = np.zeros((2160,4320),'float32')
-    mask_3[x,y] = 1
+    #-----------------------------------------------
+    #  Make save array
+    #-----------------------------------------------
 
+    # mask array for saving
+    mask = np.zeros((2160,4320),'float32')
+    mask[x,y] = 1
+
+    # copy file of mask array
+    f_mask = np.zeros((2160,4320),'float32')
+
+    #-----------------------------------------------
+    #  Make explore array
+    #-----------------------------------------------
+
+    # explore file
     search_1 = np.zeros((2160,4320),'float32')
     search_1[x,y] = 1
     search_2 = np.zeros((2160,4320),'float32')
     search_2[x,y] = 1
+
+    #-----------------------------------------------
+    #  Count total target search grids
+    #-----------------------------------------------
+
+    # number of grids for search in initial stage
     for a in range(x-1, x+2):
         for b in range(y-1, y+2):
             search_2[a,b] = 1
-    judgement = np.sum(search_2-search_1)
-    ran = 1
-
-    f_mask_1 = np.zeros((2160,4320),'float32')
-    f_mask_2 = np.zeros((2160,4320),'float32')
-    f_mask_3 = np.zeros((2160,4320),'float32')
+    search_total_num = np.sum(search_2-search_1)
 
     #-----------------------------------------------
-    roop = 1
-    while judgement>0 and roop<8:
-        for a in range(0,2160,1):
-            for b in range(0,4320,1):
-                if search_2[a,b]==1:
-                    if pop[a,b]>thre_1:
-                        if mask_1[a-1,b-1]==1 or mask_1[a,b-1]==1 or mask_1[a+1,b-1]==1 or mask_1[a-1,b]==1 or mask_1[a+1,b]==1 or mask_1[a-1,b+1]==1 or mask_1[a,b+1]==1 or mask_1[a+1,b+1]==1:
-                            mask_1[a,b] = 1
-                    elif pop[a,b]>thre_2:
-                        if mask_2[a-1,b-1]==1 or mask_2[a,b-1]==1 or mask_2[a+1,b-1]==1 or mask_2[a-1,b]==1 or mask_2[a+1,b]==1 or mask_2[a-1,b+1]==1 or mask_2[a,b+1]==1 or mask_2[a+1,b+1]==1:
-                            mask_2[a,b] = 1
-                    elif pop[a,b]>thre_3:
-                        if mask_3[a-1,b-1]==1 or mask_3[a,b-1]==1 or mask_3[a+1,b-1]==1 or mask_3[a-1,b]==1 or mask_3[a+1,b]==1 or mask_3[a-1,b+1]==1 or mask_3[a,b+1]==1 or mask_3[a+1,b+1]==1:
-                            mask_3[a,b] = 1
+    #  Explore start
+    #-----------------------------------------------
 
-        ran = ran + 1
+    while search_total_num>0 and radius<radius_max:
+        print(f"################## radius = {radius} ##################")
+        for a in range(2160):
+            for b in range(4320):
+                if search_2[a,b]==1:
+                    if pop[a,b]>threshold and radius<radius_max:
+                        # explore match grid
+                        if mask[a-1,b-1]==1 or mask[a,b-1]==1 or mask[a+1,b-1]==1 or mask[a-1,b]==1 or mask[a+1,b]==1 or mask[a-1,b+1]==1 or mask[a,b+1]==1 or mask[a+1,b+1]==1:
+                            mask[a,b] = 1
+
+                        # evaluate coverage
+                        mp = mask*pop
+                        pop_mask = np.sum(mp)
+                        coverage = float(pop_mask/pop_city)
+
+                        # stop exploring
+                        if coverage >= 1.0:
+                            radius = radius_max
+
+
+        # update radius
+        radius += 1
+
+        # update search_1
         search_1 = np.copy(search_2)
-        for c in range(x-ran,x+ran+1,1):
-            for d in range(y-ran,y+ran+1,1):
+
+        # search area with updated radius
+        for c in range(x-radius, x+radius+1):
+            for d in range(y-radius, y+radius+1):
                 search_2[c,d] = 1
+
+        # diffirence between previous radius and updated radius
         search_2 = search_2-search_1
 
-        judgement_1 = np.sum(mask_1-f_mask_1)
-        f_mask_1 = np.copy(mask_1)
-        judgement_2 = np.sum(mask_2-f_mask_2)
-        f_mask_2 = np.copy(mask_2)
-        judgement_3 = np.sum(mask_3-f_mask_3)
-        f_mask_3 = np.copy(mask_3)
+        # increament in masks
+        search_total_num = np.sum(mask-f_mask)
 
-        JG = [judgement_1,judgement_2,judgement_3]
-        judgement = max(JG)
-
-        roop += 1
-    #-----------------------------------------------
-
-    mp_1 = mask_1*pop
-    pop_mask_1 = np.sum(mp_1)
-    mp_2 = mask_2*pop
-    pop_mask_2 = np.sum(mp_2)
-    mp_3 = mask_3*pop
-    pop_mask_3 = np.sum(mp_3)
-
-    pop_mask_list = [pop_mask_1,pop_mask_2,pop_mask_3]
-    coverage = [float(M/pop_city) for M in pop_mask_list]
-    evl = [abs(C-1.0) for C in coverage]
-    min_evl = min(evl)
-    best_index = evl.index(min_evl)
-    best_coverage = coverage[best_index]
-
-#-------------------ROUND 2----------------------
-    if best_index==0:
-        tt_4 = 500
-        tt_best = 1000
-        tt_5 = 1500
-        pop_mask_best = pop_mask_list[0]
-        best_mask = mask_1
-    elif best_index==1:
-        tt_4 = 1500
-        tt_best = 2000
-        tt_5 = 2500
-        pop_mask_best = pop_mask_list[1]
-        best_mask = mask_2
-    elif best_index==2:
-        tt_4 = 2500
-        tt_best = 3000
-        tt_5 = 3500
-        pop_mask_best = pop_mask_list[2]
-        best_mask = mask_3
-
-    tt_list = [tt_4,tt_best,tt_5]
-    thre_4 = tt_4*A
-    thre_5 = tt_5*A
-    thre_list = [tt_4*A,tt_best*A,tt_5*A]
-
-    mask_4 = np.zeros((2160,4320),'float32')
-    mask_4[x,y] = 1
-    mask_5 = np.zeros((2160,4320),'float32')
-    mask_5[x,y] = 1
-
-    search_1 = np.zeros((2160,4320),'float32')
-    search_1[x,y] = 1
-    search_2 = np.zeros((2160,4320),'float32')
-    search_2[x,y] = 1
-    for a in range(x-1,x+2):
-        for b in range(y-1,y+2):
-            search_2[a,b] = 1
-    judgement = np.sum(search_2-search_1)
-    ran = 1
-
-    f_mask_4 = np.zeros((2160,4320),'float32')
-    f_mask_5 = np.zeros((2160,4320),'float32')
+        # update mask copy
+        f_mask = np.copy(mask)
 
     #-----------------------------------------------
-    roop = 1
-    while judgement>0 and roop<8:
-        for a in range(0,2160,1):
-            for b in range(0,4320,1):
-                if search_2[a,b]==1:
-                    if pop[a,b]>thre_4:
-                        if mask_4[a-1,b-1]==1 or mask_4[a,b-1]==1 or mask_4[a+1,b-1]==1 or mask_4[a-1,b]==1 or mask_4[a+1,b]==1 or mask_4[a-1,b+1]==1 or mask_4[a,b+1]==1 or mask_4[a+1,b+1]==1:
-                            mask_4[a,b] = 1
-                    elif pop[a,b]>thre_5:
-                        if mask_5[a-1,b-1]==1 or mask_5[a,b-1]==1 or mask_5[a+1,b-1]==1 or mask_5[a-1,b]==1 or mask_5[a+1,b]==1 or mask_5[a-1,b+1]==1 or mask_5[a,b+1]==1 or mask_5[a+1,b+1]==1:
-                            mask_5[a,b] = 1
-
-        ran = ran + 1
-        search_1 = np.copy(search_2)
-        for c in range(x-ran,x+ran+1,1):
-            for d in range(y-ran,y+ran+1,1):
-                search_2[c,d] = 1
-        search_2 = search_2-search_1
-
-        judgement_4 = np.sum(mask_4-f_mask_4)
-        f_mask_4 = np.copy(mask_4)
-        judgement_5 = np.sum(mask_5-f_mask_5)
-        f_mask_5 = np.copy(mask_5)
-
-        JG = [judgement_4,judgement_5]
-        judgement = max(JG)
-
-        roop += 1
+    # Output result
     #-----------------------------------------------
 
-    mp_4 = mask_4*pop
-    pop_mask_4 = np.sum(mp_4)
-    mp_5 = mask_5*pop
-    pop_mask_5 = np.sum(mp_5)
-
-    pop_mask_list_2 = [pop_mask_4,pop_mask_best,pop_mask_5]
-    coverage_2 = [float(M/pop_city) for M in pop_mask_list_2]
-    evl_2 = [abs(C-1.0) for C in coverage_2]
-    min_evl_2 = min(evl_2)
-    best_index_2 = evl_2.index(min_evl_2)
-    best_coverage_2 = coverage_2[best_index_2]
-    pop_best_2 = pop_mask_list_2[best_index_2]
-    tt_best_2 = tt_list[best_index_2]
-    thre_best_2 = thre_list[best_index_2]
-
-#-------------------ROUND 3----------------------
-    if best_index_2==0:
-        best_mask_2 = mask_4
-    elif best_index_2==1:
-        best_mask_2 = best_mask
-    elif best_index_2==2:
-        best_mask_2 = mask_5
-
-    tt_6 = tt_best_2-250
-    tt_7 = tt_best_2+250
-
-    tt_list = [tt_6,tt_best_2,tt_7]
-    thre_6 = tt_6*A
-    thre_7 = tt_7*A
-    thre_list = [thre_6,thre_best_2,thre_7]
-
-    mask_6 = np.zeros((2160,4320),'float32')
-    mask_6[x,y] = 1
-    mask_7 = np.zeros((2160,4320),'float32')
-    mask_7[x,y] = 1
-
-    search_1 = np.zeros((2160,4320),'float32')
-    search_1[x,y] = 1
-    search_2 = np.zeros((2160,4320),'float32')
-    search_2[x,y] = 1
-    for a in range(x-1,x+2):
-        for b in range(y-1,y+2):
-            search_2[a,b] = 1
-    judgement = np.sum(search_2-search_1)
-    ran = 1
-
-    f_mask_6 = np.zeros((2160,4320),'float32')
-    f_mask_7 = np.zeros((2160,4320),'float32')
-
-    roop = 1
-    while judgement>0 and roop<8:
-        for a in range(0,2160,1):
-            for b in range(0,4320,1):
-                if search_2[a,b]==1:
-                    if pop[a,b]>thre_6:
-                        if mask_6[a-1,b-1]==1 or mask_6[a,b-1]==1 or mask_6[a+1,b-1]==1 or mask_6[a-1,b]==1 or mask_6[a+1,b]==1 or mask_6[a-1,b+1]==1 or mask_6[a,b+1]==1 or mask_6[a+1,b+1]==1:
-                            mask_6[a,b] = 1
-                    elif pop[a,b]>thre_7:
-                        if mask_7[a-1,b-1]==1 or mask_7[a,b-1]==1 or mask_7[a+1,b-1]==1 or mask_7[a-1,b]==1 or mask_7[a+1,b]==1 or mask_7[a-1,b+1]==1 or mask_7[a,b+1]==1 or mask_7[a+1,b+1]==1:
-                            mask_7[a,b] = 1
-
-        ran = ran + 1
-        search_1 = np.copy(search_2)
-        for c in range(x-ran,x+ran+1,1):
-            for d in range(y-ran,y+ran+1,1):
-                search_2[c,d] = 1
-        search_2 = search_2-search_1
-
-        judgement_6 = np.sum(mask_6-f_mask_6)
-        f_mask_6 = np.copy(mask_6)
-        judgement_7 = np.sum(mask_7-f_mask_7)
-        f_mask_7 = np.copy(mask_7)
-        JG = [judgement_6,judgement_7]
-        judgement = max(JG)
-
-        roop += 1
-    #-----------------------------------------------
-
-    mp_6 = mask_6*pop
-    pop_mask_6 = np.sum(mp_6)
-    mp_7 = mask_7*pop
-    pop_mask_7 = np.sum(mp_7)
-
-    pop_mask_list_3 = [pop_mask_6,pop_best_2,pop_mask_7]
-    coverage_3 = [float(M/pop_city) for M in pop_mask_list_3]
-    evl_3 = [abs(C-1.0) for C in coverage_3]
-    min_evl_3 = min(evl_3)
-    best_index_3 = evl_3.index(min_evl_3)
-    best_coverage_3 = coverage_3[best_index_3]
-    pop_best_3 = pop_mask_list_3[best_index_3]
-    tt_best_3 = tt_list[best_index_3]
-    thre_best_3 = thre_list[best_index_3]
-
-    #-----------------------------------------------
-
-    if best_index_3==0:
-        best_mask_3 = mask_6
-    elif best_index_3==1:
-        best_mask_3 = best_mask_2
-    elif best_index_3==2:
-        best_mask_3 = mask_7
-
-    grid_num = np.sum(best_mask_3)
-    city_A = best_mask_3*A
+    grid_num = np.sum(mask)
+    city_A = mask*A
     city_area = np.sum(city_A)
 
-#------------------------------------------------
-    if tt_best_3 != 3750 and tt_best_3 != 250 and 0.8<=best_coverage_3<=1.2:
-        print(f"citynum {num}\n" \
-              f"threshold_1 {tt_best_3}\n" \
-              f"threshold_2 {thre_best_3}\n" \
-              f"pop_mask {pop_best_3}\n" \
-              f"pop_city {pop_city}\n" \
-              f"coverage {best_coverage_3}\n" \
-              f"OK grid_num {grid_num}\n" \
-              f"city_area {city_area}\n" \
-              f"{city_name}\n")
-    elif tt_best_3 != 3750 and tt_best_3 != 250:
-        print(f"citynum {num}\n" \
-              f"threshold_1 {tt_best_3}\n" \
-              f"threshold_2 {thre_best_3}\n" \
-              f"pop_mask {pop_best_3}\n" \
-              f"pop_city {pop_city}\n" \
-              f"coverage {best_coverage_3}\n" \
-              f"NG grid_num {grid_num}\n" \
-              f"city_area {city_area}\n" \
-              f"{city_name}\n")
-    elif tt_best_3 == 3750:
-        print(f"citynum {num}\n" \
-              f"threshold_1 {tt_best_3}\n" \
-              f"threshold_2 {thre_best_3}\n" \
-              f"pop_mask {pop_best_3}\n" \
-              f"pop_city {pop_city}\n" \
-              f"coverage {best_coverage_3}\n" \
-              f"high grid_num {grid_num}\n" \
-              f"city_area {city_area}\n" \
-              f"{city_name}\n")
-    elif tt_best_3 == 250:
-        if 0.8<=best_coverage_3<=1.2:
-            print(f"citynum {num}\n" \
-                  f"threshold_1 {tt_best_3}\n" \
-                  f"threshold_2 {thre_best_3}\n" \
-                  f"pop_mask {pop_best_3}\n" \
-                  f"pop_city {pop_city}\n" \
-                  f"coverage {best_coverage_3}\n" \
-                  f"low_OK grid_num {grid_num}\n" \
-                  f"city_area {city_area}\n" \
-                  f"{city_name}\n")
-        else:
-            print(f"citynum {num}\n" \
-                  f"threshold_1 {tt_best_3}\n" \
-                  f"threshold_2 {thre_best_3}\n" \
-                  f"pop_mask {pop_best_3}\n" \
-                  f"pop_city {pop_city}\n" \
-                  f"coverage {best_coverage_3}\n" \
-                  f"low_NG grid_num {grid_num}\n" \
-                  f"city_area {city_area}\n" \
-                  f"{city_name}\n")
+    print(f"cityindex {index}\n" \
+          f"threshold {threshold}\n" \
+          f"explored_pop {pop_mask}\n" \
+          f"true_pop {pop_city}\n" \
+          f"coverage {coverage}\n" \
+          f"city_mask {grid_num}\n" \
+          f"city_area {city_area}\n" \
+          f"{city_name}\n")
 
-    name = f'../../map/dat/cty_msk_/txt/city_{num}.txt'
+    return city_name, mask, grid_num, coverage, threshold_density
 
-#------------------------------------------------
 
-    ff = open(name,'w')
-    for l in range(0,2160,1):
-        line = best_mask_3[l,:]
+def thres_loop(index):
+
+    #------------------------------------------------
+    # Initialization
+    #------------------------------------------------
+
+    error_rate = 0.01
+    threshold_lst = np.arange(100, 2000, 100)
+    mask_lst = []
+    grid_lst = []
+    coverage_lst = []
+    threshold_density_lst = []
+
+    #------------------------------------------------
+    # threshold loop
+    #------------------------------------------------
+
+    for i in threshold_lst:
+        print(f"threshold {i}")
+        city_name, mask, grid_num, coverage, threshold_density = explore_citymask(index, i)
+        mask_lst.append(mask)
+        grid_lst.append(grid_num)
+        coverage_lst.append(coverage)
+        threshold_density_lst.append(threshold_density)
+
+    #------------------------------------------------
+    # Threshold loop
+    #------------------------------------------------
+
+    low_errors =  [index for index, value in enumerate(coverage_lst) if np.abs(1-value) < error_rate]
+    grid_array = np.array(grid_lst)[low_errors]
+    mask_array = np.array(mask_lst)[low_errors]
+    coverage_array = np.array(coverage_lst)[low_errors]
+    threshold_density_array = np.array(threshold_density_lst)[low_errors]
+
+    #------------------------------------------------
+    # Minumim grid_num
+    #------------------------------------------------
+
+    best_index = np.argmin(grid_array)
+    best_grid = grid_array[best_index]
+    best_mask = mask_array[best_index]
+    best_coverage = coverage_array[best_index]
+    best_threshold = threshold_density_array[best_index]
+
+    print(best_threshold, best_grid, best_coverage)
+
+    #------------------------------------------------
+    # SAVE FILE
+    #------------------------------------------------
+
+    maskpath = f'/home/kajiyama/H08/H08_20230612/map/dat/cty_msk_/txt/city_{index}.txt'
+
+    ff = open(maskpath,'w')
+    for l in range(0,2160):
+        line = best_mask[l,:]
         aaa = line.tolist()
         aaa = str(aaa)
         aa1 = aaa.strip("[")
@@ -353,3 +231,17 @@ for num in range(1,901,1):   ##cheak city number##
         aa2 = aa2.strip(",")
         ff.write("\n%s"%aa2)
     ff.close()
+
+    resultpath = f'/home/kajiyama/H08/H08_20230612/map/dat/result_citymask.txt'
+
+    with open(resultpath, 'w') as file:
+        file.write(f"{city_name}, {best_threshold}, {best_coverage}, {best_grid}\n")
+
+
+def main():
+    for index in range(1, 901):
+        thres_loop(index)
+
+
+if __name__ == '__main__':
+    main()

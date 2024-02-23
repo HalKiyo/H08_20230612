4fb31c6d2065be24c118ba0177ccbca1d80a71db
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2024-02-23 23:50:50 +0900

    josui uploaded

--- map/dat/explore_josui_gesui.py
+++ map/dat/explore_josui_gesui.py
@@ -1,5 +1,7 @@
 import os
+import copy
 import numpy as np
+import matplotlib.pyplot as plt
 
 #---------------------------------------------------------------------------------------------------------------
 # MODULES
@@ -14,12 +16,16 @@ def l_coordinate_to_tuple(lcoordinate, a=2160, b=4320):
 # Main function
 #---------------------------------------------------------------------------------------------------------------
 
-def explore(target_index, remove_grid, innercity_grid, width):
+def explore(target_index, remove_grid, innercity_grid, width, save_flag=False):
+    """
+    A: After over remove_grid process
+    B: After over remove_grid process & over innercity_grid process
+    """
     latgrd = 2160 # sum of latitude grids (y)
     longrd = 4320 # sum of longitude grids (x)
 
 #---------------------------------------------------------------------------------------------------------------
-# PATH
+#   PATH
 #---------------------------------------------------------------------------------------------------------------
 
     # root directory
@@ -170,8 +176,8 @@ def explore(target_index, remove_grid, innercity_grid, width):
     g_rivnum_cropped_masked = np.ma.masked_array(g_rivnum_cropped, np.isnan(g_rivnum_cropped))
 
     # マスクされていない要素(Nanじゃない値)のユニークな値とその出現回数を取得
-    unique_values, counts = np.unique(g_rivnum_cropped_masked.compressed(), return_counts=True)
-    value_counts_dict = dict(zip(unique_values, counts))
+    unique_values_org, counts_org = np.unique(g_rivnum_cropped_masked.compressed(), return_counts=True)
+    value_counts_dict = dict(zip(unique_values_org, counts_org))
 
     # 値（個数）の多い順にソート
     # 都市マスク内の流域番号で，出現回数が多い順に並んでいるはず
@@ -192,8 +198,7 @@ def explore(target_index, remove_grid, innercity_grid, width):
         # これらの位置に新しい配列にrivnumを設定
         Rivnum_A_array[matching_positions] = rivnum_id
 
-    # 0 or 非有限数の要素をマスクする
-    # Rivnum_B_arrayは都市マスクなしのすべての流域
+    # Rivnum_A_arrayは都市マスクなしのすべての流域
     Rivnum_A_array = np.ma.masked_where(~np.isfinite(Rivnum_A_array) | (Rivnum_A_array == 0), Rivnum_A_array)
 
 #---------------------------------------------------------------------------------------------------------------
@@ -206,16 +211,18 @@ def explore(target_index, remove_grid, innercity_grid, width):
     Rivnum_A_array_citymasked = np.ma.masked_where((g_mask_cropped != 1) | invalid_mask, Rivnum_A_array)
 
 #---------------------------------------------------------------------------------------------------------------
-#   rivaraを使って河口グリッドを探索する (rivara_max_array)
+#   マスクされていない要素のユニークな値とその出現回数を取得(unique_values_A)
 #---------------------------------------------------------------------------------------------------------------
 
-    # マスクされていない要素のユニークな値とその出現回数を取得
-    # 都市マスク内の流域グリッド数で出現回数が多い物を探索している
     unique_values_A, counts_A = np.unique(Rivnum_A_array_citymasked.compressed(), return_counts=True)
     value_counts_dict_A = dict(zip(unique_values_A, counts_A))
 
+#---------------------------------------------------------------------------------------------------------------
+#   rivaraを使って河口グリッドを探索する (rivara_max_array_A)
+#---------------------------------------------------------------------------------------------------------------
+
     # データ型とサイズに基づいて新しい配列を作成
-    rivara_max_array = np.ma.masked_all(g_ara_num_cropped.shape, dtype='float32')
+    rivara_max_array_A = np.ma.masked_all(g_ara_num_cropped.shape, dtype='float32')
 
     for rivnum_id in value_counts_dict_A.keys():
         # 同じrivnumの位置を取得
@@ -224,7 +231,7 @@ def explore(target_index, remove_grid, innercity_grid, width):
         max_rivara_position = np.argmax(g_rivara_cropped[matching_positions])
         # 最大のrivaraの位置に対応するrivnumを新しい配列に保存する
         # 河口グリッドに該当
-        rivara_max_array[matching_positions[0][max_rivara_position], matching_positions[1][max_rivara_position]] = rivnum_id
+        rivara_max_array_A[matching_positions[0][max_rivara_position], matching_positions[1][max_rivara_position]] = rivnum_id
 
 #---------------------------------------------------------------------------------------------------------------
 #   riv nxtl -> lonlat coordinate array 24x24x2 (riv_nxlonlat_cropped)
@@ -248,31 +255,30 @@ def explore(target_index, remove_grid, innercity_grid, width):
     riv_nxlonlat_cropped = np.array(riv_nxlonlat_lst).reshape(riv_nxlonlat_shape)
     riv_nxlonlat_cropped = riv_nxlonlat_cropped.astype(int)
 
-    # ここまでリファクタリング終わり
-
 #---------------------------------------------------------------------------------------------------------------
-#   各流域の経路座標　(results_dict)
-#   各経路が流域番号で格納され，1つのファイルに集約 (riv_path_array)
+#   各流域の経路座標　(path_dict)
+#   各経路が流域番号で格納され，1つのファイルに集約 (riv_path_array_A)
 #---------------------------------------------------------------------------------------------------------------
 
-    results_dict = {}
-    # マスク内に存在する流域ID
-    unique_ids = np.unique(rivara_max_array.compressed())
-    riv_path_array = np.ma.masked_all(rivara_max_array.shape, dtype='float32')
+    # 保存用の変数を設定
+    path_dict = {}
+    riv_path_array_A = np.ma.masked_all(rivara_max_array_A.shape, dtype='float32')
     visited_coords = set()
 
-    for uid in unique_ids:
+    # マスク内の流域IDごとにループ
+    for uid in unique_values_A:
         # 河口グリッドのインデックス
-        coords_a = np.argwhere(rivara_max_array == uid)
-        riv_path_array[coords_a[0][0], coords_a[0][1]] = uid
+        coords_a = np.argwhere(rivara_max_array_A == uid)
+        riv_path_array_A[coords_a[0][0], coords_a[0][1]] = uid
         if coords_a.size > 0:
             target_coord = tuple(coords_a[0]) 
             path_coords = [target_coord]
-            for _ in range(300):
+            for _ in range(len(g_mask_cropped)):
                 if target_coord in visited_coords:
                     break
                 visited_coords.add(target_coord)
-                # riv_nxlonlat_croppedはrivnxlのlonlat表示なので，target_coordを次のセルに指し示すrivnxlのインデックスを取得
+                # riv_nxlonlat_croppedはrivnxlのlonlat表示なので
+                # target_coordを次のセルに指し示すrivnxlのインデックスを取得
                 matched_coords = np.argwhere(np.all(target_coord == riv_nxlonlat_cropped, axis=2))
                 if len(matched_coords) == 0:
                     break
@@ -285,13 +291,111 @@ def explore(target_index, remove_grid, innercity_grid, width):
                 max_index = np.argmax(rivara_values)
                 best_coord = unvisited_matched[max_index]
                 # 河口グリッドのファイルに経路をそれぞれ足していく
-                riv_path_array[best_coord[0], best_coord[1]] = uid
+                riv_path_array_A[best_coord[0], best_coord[1]] = uid
                 target_coord = best_coord 
                 # path_coordに経路を足していく
                 path_coords.append(target_coord)
 
             # 各流域の経路を保存
-            results_dict[uid] = path_coords
+            path_dict[uid] = path_coords
+
+#---------------------------------------------------------------------------------------------------------------
+#   Rivpath over innercity_grid (riv_path_city_B)
+#---------------------------------------------------------------------------------------------------------------
+
+    # city mask
+    fill_value = 1e20
+    riv_path_array_filled = riv_path_array_A.filled(fill_value)
+    riv_path_city_A = np.where(g_mask_cropped==1, riv_path_array_filled, fill_value)
+
+    # make new array
+    riv_path_city_B = copy.deepcopy(riv_path_city_A)
+
+    for uid in unique_values_A:
+        count = 0
+        mask = (riv_path_city_A == uid)
+        count = np.sum(mask)
+        # もし主河道のセル数が都市マスク内で指定の値より少ない場合削除
+        if count < innercity_grid:
+            riv_path_city_B[riv_path_city_B== uid] = fill_value
+
+    riv_path_city_B = np.ma.masked_where(riv_path_city_B >= fill_value, riv_path_city_B)
+
+#---------------------------------------------------------------------------------------------------------------
+#   Update unique river basin number after 2 removing process (unique_values_B)
+#---------------------------------------------------------------------------------------------------------------
+
+    unique_values_B, _ = np.unique(riv_path_city_B, return_counts=True)
+
+#---------------------------------------------------------------------------------------------------------------
+#   都市マスク内に存在する流域を全範囲で取得(Rivnum_B_array)
+#---------------------------------------------------------------------------------------------------------------
+
+    # 新しい配列を作成
+    Rivnum_B_array = np.ma.masked_all(g_rivnum_cropped_masked.shape, dtype='float32')
+
+    # Rivnum_A_arrayに存在する新しいunique_id地点のみを保存
+    for uid in unique_values_B:
+        row_indices, col_indices = np.where(Rivnum_A_array == uid)
+        Rivnum_B_array[row_indices, col_indices] = uid
+
+#---------------------------------------------------------------------------------------------------------------
+#   Updated river mouse grid (rivara_max_array_B)
+#---------------------------------------------------------------------------------------------------------------
+
+    # データ方とサイズに基づいて新しい配列を作成
+    rivara_max_array_B = np.ma.masked_all(g_ara_num_cropped.shape, dtype='float32')
+
+    for rivnum_id in unique_values_B:
+        # 同じrivnumの位置を取得
+        matching_positions = np.where(Rivnum_A_array_citymasked == rivnum_id)
+        # これらの位置におけるrivaraの最大値の位置を取得
+        max_rivara_position = np.argmax(g_rivara_cropped[max_rivara_position])
+        # 最大のrivaraの位置に対応するrivnumを新しい配列に保存する
+        rivara_max_array_B[matching_positions[0][max_rivara_position], matching_positions[1][max_rivara_position]] = rivnum_id
+
+#---------------------------------------------------------------------------------------------------------------
+#   Update riv_path_array with full length out of city mask (riv_path_array_B)
+#---------------------------------------------------------------------------------------------------------------
+
+    # 保存用の変数を設定
+    path_dict = {}
+    riv_path_array_B = np.ma.masked_all(rivara_max_array_B.shape, dtype='float32')
+    visited_coords = set()
+
+    # マスク内の流域IDごとにループ
+    for uid in unique_values_B:
+        # 河口グリッドのインデックス
+        coords_a = np.argwhere(rivara_max_array_B == uid)
+        riv_path_array_B[coords_a[0][0], coords_a[0][1]] = uid
+        if coords_a.size > 0:
+            target_coord = tuple(coords_a[0]) 
+            path_coords = [target_coord]
+            for _ in range(len(g_mask_cropped)):
+                if target_coord in visited_coords:
+                    break
+                visited_coords.add(target_coord)
+                # riv_nxlonlat_croppedはrivnxlのlonlat表示なので
+                # target_coordを次のセルに指し示すrivnxlのインデックスを取得
+                matched_coords = np.argwhere(np.all(target_coord == riv_nxlonlat_cropped, axis=2))
+                if len(matched_coords) == 0:
+                    break
+                # マッチしたインデックスの中でrivaraが最大のものを選ぶ
+                unvisited_matched = [tuple(coord) for coord in matched_coords if tuple(coord) not in visited_coords]
+                if not unvisited_matched:
+                    break
+                # g_rivara_croppedに座標をいれて，最大最小を比べている
+                rivara_values = [g_rivara_cropped[coord[0], coord[1]] for coord in unvisited_matched]
+                max_index = np.argmax(rivara_values)
+                best_coord = unvisited_matched[max_index]
+                # 河口グリッドのファイルに経路をそれぞれ足していく
+                riv_path_array_B[best_coord[0], best_coord[1]] = uid
+                target_coord = best_coord 
+                # path_coordに経路を足していく
+                path_coords.append(target_coord)
+
+            # 各流域の経路を保存
+            path_dict[uid] = path_coords
 
 #---------------------------------------------------------------------------------------------------------------
 #   Explore josui grids (josui_lst)
@@ -300,17 +404,10 @@ def explore(target_index, remove_grid, innercity_grid, width):
     # determine josui place
     josui_lst = []
 
-    # get uid
-    tmp_id_lst = list(results_dict.keys())
-    print(tmp_id_lst)
-
-    # city mask
-    tmp_uid_masked = np.where(g_mask_cropped==1, riv_path_array, np.nan)
-
     # loop uid
-    for key_num in tmp_id_lst:
+    for key_num in unique_values_B:
         # get river path
-        indices = np.argwhere(tmp_uid_masked == key_num)
+        indices = np.argwhere(riv_path_city_B == key_num)
         # get minmum river area
         rivara_values = [g_rivara_cropped[coord[0], coord[1]] for coord in indices]
         min_arg = np.argmin(rivara_values)
@@ -323,16 +420,19 @@ def explore(target_index, remove_grid, innercity_grid, width):
 #---------------------------------------------------------------------------------------------------------------
 
     # 浄水場情報を24x24のマスクファイルに保存
-    josui_array = np.ma.masked_all(g_ara_num_cropped.shape, dtype='float32')
+    josui_array = np.ma.masked_all(rivara_max_array_B.shape, dtype='float32')
 
-    for matching_position, uid in zip(josui_lst, tmp_id_lst):
+    for matching_position, uid in zip(josui_lst, unique_values_B):
         josui_array[matching_position[0], matching_position[1]] = uid
 
 #---------------------------------------------------------------------------------------------------------------
 #   Save file (josui_array)
 #---------------------------------------------------------------------------------------------------------------
-    # croppするときは必ずひっくり返す
-    # 保存・描写するときにもとに戻す
+
+    """
+    croppするときは必ずひっくり返す
+    保存・描写するときにもとに戻す
+    """
 
     # 保存用ファイル作成
     josui_for_save = np.ma.masked_all(g_rivara.shape, dtype='float32')
@@ -344,12 +444,25 @@ def explore(target_index, remove_grid, innercity_grid, width):
     josui_for_save = np.ma.filled(josui_for_save, fill_value=0)
     josui_for_save = np.where(josui_for_save > 0, 1, josui_for_save)
 
+    # debug用
+    josui_cropped = josui_for_save[lat_start:lat_end, lon_start:lon_end]
+    josui_cropped = np.flipud(josui_cropped)
+    plt.imshow(josui_cropped)
+    plt.show()
+
     # 保存するときは世界地図をひっくり返して，正しい向きにしておく
+    josui_for_save = np.flipud(josui_for_save)
+
     # city purification plant
     save_path = f'/home/kajiyama/H08/H08_20230612/map/dat/cty_prf_/city_{city_num:08d}.gl5'
-    josui_for_save = np.flipud(josui_for_save)
-    #josui_for_save.astype(np.float32).tofile(save_path)
-    #print(f"{save_path} saved")
+
+    # save_flag
+    if save_flag is True:
+        josui_for_save.astype(np.float32).tofile(save_path)
+        print(f"{save_path} saved")
+    else:
+        print('save_flag is false')
+
 
 #---------------------------------------------------------------------------------------------------------------
 # Main loop

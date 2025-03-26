666b9fd7a6965b1c596270a43af007317a41ca1a
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2025-03-26 15:15:15 +0900

    bugfixed

--- met/pre/kk_count_files.sh
+++ met/pre/kk_count_files.sh
@@ -2,16 +2,16 @@
 
 # === setting === #
 VARS="LWdown__ SWdown__ Prcp____ Snowf___ PSurf___ Qair____ Wind____ Tair____ Rainf___"
-YEAR="1989"
+YEAR="2001"
 START_DATE="0101"
 END_DATE="1231"
 PREFIX="W5E5____"
 SUFFIX=".gl5"
 
 # === main === #
-current_date="${YEAR}${START_DATE}"
 
 for VAR in $VARS; do
+    current_date="${YEAR}${START_DATE}"
     while [ "$current_date" -le "${YEAR}${END_DATE}" ]; do
         DIR="/home/kajiyama/H08/H08_20230612/met/dat/${VAR}"
         file_path="${DIR}/${PREFIX}${current_date}${SUFFIX}"
